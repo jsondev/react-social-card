d29b5a3318209a976746fb75172c752073250577
jsondev <james.anderson@thedevcode.com>
2020-04-14 11:32:44 -0400

    Added components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./App.css";
 import CardHeader from "./components/CardHeader/CardHeader";
+import CardFooter from "./components/CardFooter/CardFooter";
+import Card from "./components/Card/Card";
 
 function App() {
   return (
     <div className="App">
       <div>Icon</div>
-      <CardHeader></CardHeader>
-      <div>Card</div>
-      <div>Social Buttons</div>
+      <CardHeader user="jsondev"></CardHeader>
+      <Card></Card>
+      <CardFooter></CardFooter>
     </div>
   );
 }
