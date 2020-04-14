76e3d2346b10d94dfcf0a7ec6951576dd9256295
Anderson <James.Anderson@appliedis.com>
2020-04-14 09:26:29 -0400

    Started mockups of components

---
+++ src/components/CardHeader/CardHeader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CardHeader = () => (
+    <ul>
+        <li>The Practical Dev <span>@ThePracticalDev * Sep 10</span></li>
+        <li>Learning React? Start Small.</li>
+        <li></li>
+    </ul>
+)
+
+
+
+export default CardHeader;
